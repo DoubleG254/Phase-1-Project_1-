ca00a023338ae26b4e5ef73973be486bde97fa87
DoubleG254 <glorygweth6@gmail.com>
2023-07-08 23:31:18 +0300

    first game update

--- G3_Games/index.js
+++ G3_Games/index.js
@@ -1,22 +1,34 @@
-document.addEventListener("DOMContentLoaded");
+document.addEventListener("DOMContentLoaded", fetchData);
+
 function gameList(games) {
   let lister = document.querySelector("#games");
   let game = document.createElement("div");
+  game.className="game"
   game.innerHTML = `
-    <img src="${games.image}alt="game image" id="gamage">
-    <h2>${games.name}
-    `;
-  game.addEventListener("mouseover", () => {
-    game.innerHTML = `
-           <h2>System Requirements</h2>
-           <p>Storage : ${games.systemrequirements.storage}</p>
-           <p>RAM : ${games.systemrequirements.RAM}</p>
-           <p>CPU: ${games.systemrequirements.CPU}`;
-  });
-  game.addEventListener("mouseout", () => {
-    game.innerHTML = `
-        <img src="${games.image}alt="game image" id="gamage">
-        <h2>${games.name}
-        `;
-  });
+    <img src="${games.image}" alt="game image" id="gamage">
+    <h2>${games.name}</h2>
+    <button id="declare">System Requirement</button>
+    <button>Add to Cart</button>
+  `;
+  lister.appendChild(game);
+  let declare=document.querySelector("#declare")
+  declare.addEventListener("click",()=>{
+      let system = document.createElement("div")
+      system.className="system"
+      system.innerHTML=`
+      <p> CPU : ${games.cpu}</p>
+      <p>RAM : ${games.ram}</p>
+      <p>Storage : ${games.storage}</p>`
+      game.appendChild(system)
+  })
+ 
 }
+
+function fetchData() {
+  fetch("http://localhost:3000/games")
+    .then(resp => resp.json())
+    .then(games => {
+      games.forEach(game => gameList(game));
+    });
+}
+
