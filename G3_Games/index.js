65f7e60fae944521b1cb8a563fc7714e1d14b43b
DoubleG254 <glorygweth6@gmail.com>
2023-07-09 23:30:43 +0300

    cart event

--- G3_Games/index.js
+++ G3_Games/index.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", fetchData);
 
 function gameList(games) {
+//Display games 
   let lister = document.querySelector("#games");
   let game = document.createElement("div");
   game.className="game"
   game.innerHTML = `
     <img src="${games.image}" alt="game image" class="gamage">
     <h2>${games.name}</h2>
-    <button class="declare">System Requirement</button>
-    <button>Add to Cart</button>
+    <button class="cartAdd">Add to Cart</button>
   `;
   lister.appendChild(game);
-  let declareButtons=document.querySelectorAll(".declare")
-  declareButtons.forEach((declareButton)=>{
+  //Dispaly sysytem requirements for the games
+  let declareButton=document.querySelector(".declare")
     declareButton.addEventListener("click",()=>{
       let system = document.createElement("div")
       system.className="system"
@@ -22,13 +22,18 @@ function gameList(games) {
       <p>Storage : ${games.storage}</p>`
       game.appendChild(system)
   })
-  })
-   
-  
-  
- 
-}
+//Add game to the cart
+let cartAdd=game.querySelector(".cartAdd")
+  cartAdd.addEventListener("click",()=>{
+    let li = document.createElement("li")
 
+    li.innerHTML=`${games.name}`
+    let ul= document.querySelector(".list")
+    ul.appendChild(li)
+
+  })
+  }
+//Fetch game data from the server
 function fetchData() {
   fetch("http://localhost:3000/games")
     .then(resp => resp.json())
@@ -36,4 +41,6 @@ function fetchData() {
       games.forEach(game => gameList(game));
     });
 }
+
+
 
